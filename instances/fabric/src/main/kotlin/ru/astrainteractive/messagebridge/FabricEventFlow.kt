1e9848cfc846a0895907874300fe7c1a5f0d2c86
Roman Makeev <makeevrserg@gmail.com>
2025-07-28 18:49:45 +0300

    add sample into events

--- instances/fabric/src/main/kotlin/ru/astrainteractive/messagebridge/FabricEventFlow.kt
+++ instances/fabric/src/main/kotlin/ru/astrainteractive/messagebridge/FabricEventFlow.kt
@@ -1,16 +1,11 @@
 package ru.astrainteractive.messagebridge
 
 import kotlinx.coroutines.channels.ProducerScope
-import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.flow.channelFlow
 import kotlinx.coroutines.launch
-import net.fabricmc.fabric.api.event.Event
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents
-import java.lang.reflect.InvocationHandler
-import java.lang.reflect.Proxy
 
 
 // data
@@ -29,6 +24,21 @@ fun <T> fabricEventFlow(block: suspend FabricEventProducerScope<T>.() -> Unit): 
     block.invoke(scope)
 }
 
+/**
+ * ```
+ * val START_SERVER_TICK = fabricEventFlow {
+ *   val callback = ServerTickEvents.StartTick(::send)
+ *   ServerTickEvents.START_SERVER_TICK.register(callback)
+ * }
+ * ```
+ *
+ * ```
+ * val JOIN = fabricEventFlow {
+ *     val callback = ServerPlayConnectionEvents.Join(::send)
+ *     ServerPlayConnectionEvents.JOIN.register(callback)
+ * }
+ * ```
+ */
 inline fun <reified T> FabricEventProducerScope<KFabricEventData1<T>>.send(t: T) {
     launch { send(KFabricEventData1(t)) }
 }
@@ -39,20 +49,4 @@ fun <T1, T2> FabricEventProducerScope<KFabricEventData2<T1, T2>>.send(t1: T1, t2
 
 fun <T1, T2, T3> FabricEventProducerScope<KFabricEventData3<T1, T2, T3>>.send(t1: T1, t2: T2, t3: T3) {
     launch { send(KFabricEventData3(t1, t2, t3)) }
-}
-
-// samples
-val START_SERVER_TICK = fabricEventFlow {
-    val callback = ServerTickEvents.StartTick(::send)
-    ServerTickEvents.START_SERVER_TICK.register(callback)
-}
-
-val END_SERVER_TICK = fabricEventFlow {
-    val callback = ServerTickEvents.EndTick(::send)
-    ServerTickEvents.END_SERVER_TICK.register(callback)
-}
-
-val JOIN = fabricEventFlow {
-    val callback = ServerPlayConnectionEvents.Join(::send)
-    ServerPlayConnectionEvents.JOIN.register(callback)
 }
